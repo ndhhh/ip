087d2cbcd6974066449094f0a9d3454baf019fe1
dhdh <127590328+ndhhh@users.noreply.github.com>
2025-08-17 17:16:09 +0800

    Updated for stage 1

---
+++ src/main/java/Johnny.java
@@ -0,0 +1,13 @@
+public class Johnny {
+
+    public static String greeting() {
+        String line = "__________________________________________________";
+        String name = "Johnny";
+        
+        return line + "\n" + "Hello! I'm " + name + "\nWhat can I do for you?\n" + line + "\nBye. Hope to see you soon!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Johnny.greeting());
+    }
+}
