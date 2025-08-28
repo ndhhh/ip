f38afc48a62705c8de1022c5101c830c36b21206
dhdh <127590328+ndhhh@users.noreply.github.com>
2025-08-28 22:57:09 +0800

    Fix wildcard import in Johnny

--- src/main/java/johnny/Johnny.java
+++ src/main/java/johnny/Johnny.java
@@ -1,6 +1,14 @@
 package johnny;
 
-import johnny.commands.*;
+import johnny.commands.Command;
+import johnny.commands.DeadlineCommand;
+import johnny.commands.ByeCommand;
+import johnny.commands.DeleteCommand;
+import johnny.commands.EventCommand;
+import johnny.commands.ListCommand;
+import johnny.commands.TodoCommand;
+import johnny.commands.MarkCommand;
+import johnny.commands.UnmarkCommand;
 import johnny.parser.Parser;
 import johnny.storage.Storage;
 import johnny.tasklist.TaskList;
@@ -33,7 +41,7 @@ public class Johnny {
             if (command != null) {
                 command.execute(this.tasks, this.ui, this.storage);
                 isBye = command.isBye();
-            } 
+            }
             // ui.printLine();
         }
 
@@ -41,37 +49,37 @@ public class Johnny {
     }
 
     // public void read() {
-    //     Scanner sc = new Scanner(System.in);
-    //     String input = "";
+    // Scanner sc = new Scanner(System.in);
+    // String input = "";
 
-    //     while (true)
-    //     {
-    //         input = sc.nextLine();
-    //         if (input.equals("bye")){
-    //             this.storage.save(tasks);
-    //             System.out.println(LINE + "\nBye. Hope to see you again!\n" + LINE);
-    //             break;
-    //         } else if (input.equals("list")) {
-    //             System.out.println(this.tasksToString());
-    //         } else if (input.startsWith("mark ") || input.startsWith("unmark ")) {
-    //             String[] strings = input.split(" ");
-    //             this.parseMark(strings);
-    //         } else if (input.startsWith("todo ")) {
-    //             String[] strings = input.split(" ");
-    //             this.parseTodo(strings);
-    //         } else if (input.startsWith("deadline ")) {
-    //             String[] strings = input.split("/by ");
-    //             this.parseDeadline(strings);
-    //         } else if (input.startsWith("event ")) {
-    //             this.parseEvent(input);
-    //         } else if (input.startsWith("delete ")) {
-    //             this.parseDelete(input);
-    //         } else {
-    //             System.out.println(LINE + "\n" + "Invalid command." + "\n" + LINE);
-    //         }
-    //     }
+    // while (true)
+    // {
+    // input = sc.nextLine();
+    // if (input.equals("bye")){
+    // this.storage.save(tasks);
+    // System.out.println(LINE + "\nBye. Hope to see you again!\n" + LINE);
+    // break;
+    // } else if (input.equals("list")) {
+    // System.out.println(this.tasksToString());
+    // } else if (input.startsWith("mark ") || input.startsWith("unmark ")) {
+    // String[] strings = input.split(" ");
+    // this.parseMark(strings);
+    // } else if (input.startsWith("todo ")) {
+    // String[] strings = input.split(" ");
+    // this.parseTodo(strings);
+    // } else if (input.startsWith("deadline ")) {
+    // String[] strings = input.split("/by ");
+    // this.parseDeadline(strings);
+    // } else if (input.startsWith("event ")) {
+    // this.parseEvent(input);
+    // } else if (input.startsWith("delete ")) {
+    // this.parseDelete(input);
+    // } else {
+    // System.out.println(LINE + "\n" + "Invalid command." + "\n" + LINE);
+    // }
+    // }
 
-    //     sc.close();
+    // sc.close();
     // }
 
     public static void main(String[] args) {
